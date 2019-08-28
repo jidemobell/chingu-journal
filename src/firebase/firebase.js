c70123b32dcb9bf1173576e28e3c6a8a06e01c6d
jidemobell <jidd32@gmail.com>
2019-08-28 14:12:22 +0100

    firebase with environment vars

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -7,11 +7,11 @@ import 'firebase/firestore';
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    databaseURL: REACT_APP_FIREBASE_DATABASE_URL,
-    projectId: REACT_APP_FIREBASE_PROJECT_ID,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
     // storageBucket: "",
-    messagingSenderId: REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
-    appId: REACT_APP_FIREBASE_APP_ID
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
   };
 
 firebase.initializeApp(firebaseConfig)
