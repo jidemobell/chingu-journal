cc400102288d5869b900f2c63fcc65385a06da8e
jidemobell <jidd32@gmail.com>
2019-08-31 17:02:56 +0100

    Updates

---
+++ precache-manifest.f145df285e000e38b2fba19fe12cbb1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b0b6a1dc8aa64608dbf64c48598f550",
+    "url": "/jounal/index.html"
+  },
+  {
+    "revision": "bded1b3e80fe16ff3a3f",
+    "url": "/jounal/static/css/2.62908225.chunk.css"
+  },
+  {
+    "revision": "b723aebeb1b756f1b264",
+    "url": "/jounal/static/css/main.49ac6c38.chunk.css"
+  },
+  {
+    "revision": "bded1b3e80fe16ff3a3f",
+    "url": "/jounal/static/js/2.7c3c7f07.chunk.js"
+  },
+  {
+    "revision": "b723aebeb1b756f1b264",
+    "url": "/jounal/static/js/main.7d543a37.chunk.js"
+  },
+  {
+    "revision": "74503327af98c411868c",
+    "url": "/jounal/static/js/runtime~main.f949429a.js"
+  }
+]);
